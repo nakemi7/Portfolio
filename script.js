ef3afa04b216c81d8ce778874f6250c104051016
nakemi7 <141441780+nakemi7@users.noreply.github.com>
2025-03-17 21:27:49 -0300

    fffff

--- script.js
+++
@@ -1,7 +0,0 @@
-// Script para alternar o menu de hambúrguer
-const hamburger = document.getElementById('hamburger');
-const mobileMenu = document.getElementById('mobileMenu');
-
-hamburger.addEventListener('click', () => {
-    mobileMenu.classList.toggle('active');
-});
