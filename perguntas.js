16f1bc264b400b4e538f086e412f58c51410b6c1
nakemi7 <141441780+nakemi7@users.noreply.github.com>
2025-04-15 11:33:47 -0300

    cu

---
+++ perguntas.js
@@ -0,0 +1,119 @@
+criaCartao(
+    'Sociologia',
+    'O que estuda a sociologia?',
+    'Relações sociais entre os indivíduos.'
+)
+
+criaCartao(
+    'Matematica',
+    'Ao afirmarmos que um refrigerante possui 5% de açúcar, significa que a cada 100 litros, 500 ml são de açúcar. Verdadeiro ou Falso?',
+    'Falso. Ao afirmarmos que um refrigerante possui 5% de açúcar, significa que a cada 100 litros, 5000 ml (5 litros) são de açúcar.'
+)
+
+criaCartao(
+    'Química',
+    'Um processo _______ (endotérmico/exotérmico) envolve absorção de calor.',
+    'Endotérmico.'
+)
+
+criaCartao(
+    'Língua Portuguesa',
+    'Variação linguística diatópica é o mesmo que…',
+    'Variação regional/geográfica.'
+)
+
+criaCartao(
+    'História',
+    'O que significa Mesopotâmia?',
+    'Terra entre rios (Tigre e Eufrates).'
+)
+
+criaCartao(
+    'História',
+    'Como se divide a Era Vargas?',
+    'Governo provisório; Governo constitucional; Estado novo.'
+)
+
+criaCartao(
+    'Sociologia',
+    'A cultura pode apresentar um legado material ou imaterial, verdadeiro ou falso?',
+    'Verdadeiro'
+)
+
+criaCartao(
+    'Química',
+    'Reações químicas são?',
+    'Resultado de ações entre reagentes que geralmente formam novas substâncias.'
+)
+
+criaCartao(
+    'Matematica',
+    'Trigonometria, o que estuda?',
+    'Relações entre lados e ângulos dos triângulos.'
+)
+
+criaCartao(
+    'Matematica',
+    '  Uma hora possui quantos segundos?.',
+    ' 3600.'
+)
+
+criaCartao(
+    'Química',
+    'Como se classificam as ligações entre átomos de carbono?',
+    'Sigma ou pi.'
+)
+
+criaCartao(
+    'Matematica',
+    'Qual é a fórmula da função do primeiro grau?',
+    'f(x) = ax + b.'
+)
+
+criaCartao(
+    'Sociologia',
+    'Capitalismo industrial, qual seu objetivo central?',
+    'Desenvolvimento do sistema fabril de produção.'
+)
+
+criaCartao(
+    'Língua Portuguesa',
+    'Qual a definição de substantivo?',
+    'Grupo de palavras que nomeiam objetos, seres, lugares, qualidades, ações, etc.'
+)
+
+criaCartao(
+    'Matematica',
+    'Como tangente de um ângulo é calculada?',
+    'Tan = cateto oposto/ cateto adjacente.'
+)
+
+criaCartao(
+    'Língua Portuguesa',
+    'Os verbos indicam…',
+    'Ações, estado, fenômeno da natureza ou ainda mudança de estado.'
+)
+
+criaCartao(
+    'Sociologia',
+    'Sociologia política, quais os 3 pontos centrais de sua reflexão?',
+    'Poder; Estado; Dever político.'
+)
+
+criaCartao(
+    'Química',
+    'A teoria do octeto diz',
+    'A estabilidade de um átomo é adquirida quando o mesmo apresenta possui 8 elétrons na camada de valência, ou 2 elétrons quando possui apenas uma camada.'
+)
+
+criaCartao(
+    'Sociologia',
+    'Quais a principal motivação dos movimentos sociais?',
+    'As injustiças sociais.'
+)
+
+criaCartao(
+    'Língua Portuguesa',
+    'Paródia pode ser definida como…',
+    'releitura cômica de alguma composição.'
+)
